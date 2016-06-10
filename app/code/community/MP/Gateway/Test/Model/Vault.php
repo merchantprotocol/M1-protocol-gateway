f63e37dbe7d916173c8b2464cdf3072e0ddf68d8
David Tay <david@merchantprotocol.com>
2016-06-10 09:08:42 -0400

    Added testing stubs

---
+++ app/code/community/MP/Gateway/Test/Model/Vault.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * 
+ * @category    StoreDebug
+ * @package     StoreDebug_ErrorLog
+ * @author      Store Debug <admin@storedebug.com>
+ */
+class MP_Gateway_Test_Model_Vault extends EcomDev_PHPUnit_Test_Case {
+	public function setUp() {
+
+	}
+	/**
+	 * @test
+	 */
+	public function testVault() {
+		
+		$this->assertTrue(true);
+		
+	}
+	public function tearDown() {
+
+	}
+}
